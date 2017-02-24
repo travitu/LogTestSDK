0048f76a78eb1fa4ec969f47b308a252e7c3a943
travitu <travitu84@gmail.com>
2017-02-24 22:48:26 +0900

    no message

---
+++ LogTestSDK.framework/Headers/LogTestSDK.h
@@ -0,0 +1,19 @@
+//
+//  LogTestSDK.h
+//  LogTestSDK
+//
+//  Created by Toshikazu Fukuoka on 2017/02/24.
+//  Copyright © 2017年 travitu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LogTestSDK.
+FOUNDATION_EXPORT double LogTestSDKVersionNumber;
+
+//! Project version string for LogTestSDK.
+FOUNDATION_EXPORT const unsigned char LogTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogTestSDK/PublicHeader.h>
+
+
